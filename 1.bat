b2b9cc2ce4e9f1141c893cddb212561a440ed035
Eugene <38906406+EvgBor1@users.noreply.github.com>
2020-03-10 17:27:45 +0300

    delete services

---
+++ 1.bat
@@ -0,0 +1,3 @@
+FOR %%a IN (TP_Bugzilla_3.13.10.46679 TP_Follow_3.13.10.46679 TP_GitIntegration_3.13.10.46679 TP_MashupManager_3.13.10.46679 TP_MercurialIntegration_3.13.10.46679 TP_PerforceIntegration_3.13.10.46679 ^
+TP_PopEmailIntegration_3.13.10.46679 TP_PushNotifications_3.13.10.46679 TP_RecentItems_3.13.10.46679 TP_Search2_3.13.10.46679 TP_SubversionIntegration_3.13.10.46679 TP_TaskCreator_3.13.10.46679 ^
+TP_TestRunImport_3.13.10.46679 TP_TfsIntegration_3.13.10.46679 TP_WebHooks_3.13.10.46679) do sc delete %%a
