d282d59c40c50cde416c6ee5e98ca4712034d227
Eugene <38906406+EvgBor1@users.noreply.github.com>
2020-05-22 17:01:09 +0300

    Update 1.bat

--- 1.bat
+++ 1.bat
@@ -1,3 +1,1 @@
-FOR %%a IN (TP_Bugzilla_3.13.10.46679 TP_Follow_3.13.10.46679 TP_GitIntegration_3.13.10.46679 TP_MashupManager_3.13.10.46679 TP_MercurialIntegration_3.13.10.46679 TP_PerforceIntegration_3.13.10.46679 ^
-TP_PopEmailIntegration_3.13.10.46679 TP_PushNotifications_3.13.10.46679 TP_RecentItems_3.13.10.46679 TP_Search2_3.13.10.46679 TP_SubversionIntegration_3.13.10.46679 TP_TaskCreator_3.13.10.46679 ^
-TP_TestRunImport_3.13.10.46679 TP_TfsIntegration_3.13.10.46679 TP_WebHooks_3.13.10.46679) do sc delete %%a
+foreach ($svc in (Get-Service -Name TP_*).Name) { Stop-Service -name  $svc -Force; sc.exe delete $svc}
